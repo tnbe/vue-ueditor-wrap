023e651b74200bc446ee009b47d77b5353f5c204
zhanghonghao <mayun@zhanghonghao.com>
2018-12-19 15:19:03 +0800

    dialo

--- src/components/vue-ueditor-wrap.js
+++ src/components/vue-ueditor-wrap.js
@@ -80,6 +80,57 @@ export default {
         return btn
       }, index)
     },
+    // 添加自定义弹窗
+    registerDialog: ({ name, icon, tip, handler, index, UE = window.UE }) => {
+       UE.registerUI(name, (editor, name) => {
+//创建dialog
+              const dialog = new UE.ui.Dialog({
+                  //指定弹出层中页面的路径，这里只能支持页面,因为跟addCustomizeDialog.js相同目录，所以无需加路径
+                  iframeUrl: 'customizeDialogPage.html',
+                  //需要指定当前的编辑器实例
+                  editor: editor,
+                  //指定dialog的名字
+                  name: name,
+                  //dialog的标题
+                  title: "这是个测试浮层",
+
+                  //指定dialog的外围样式
+                  cssRules: "width:600px;height:300px;",
+
+                  //如果给出了buttons就代表dialog有确定和取消
+                  buttons: [
+                      {
+                          className: 'edui-okbutton',
+                          label: '确定',
+                          onclick: function() {
+                              dialog.close(true);
+                          }
+                      },
+                      {
+                          className: 'edui-cancelbutton',
+                          label: '取消',
+                          onclick: function() {
+                              dialog.close(false);
+                          }
+                      }
+                  ]
+              });
+
+              //参考addCustomizeButton.js
+              const btn = new UE.ui.Button({
+                  name: 'dialogbutton' + name,
+                  title: 'dialogbutton' + name,
+                  //需要添加的额外样式，指定icon图标，这里默认使用一个重复的icon
+                  cssRules: 'background-position: -500px 0;',
+                  onclick: function() {
+                      //渲染dialog
+                      dialog.render();
+                      dialog.open();
+                  }
+              });
+              return btn;
+          }, index)
+      },
     // 实例化编辑器
     _initEditor () {
       this.$nextTick(() => {
